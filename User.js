861322c6c711658558b84d0021f346e65896ad66
Jefferson🔥 <160180992+Jefferson095@users.noreply.github.com>
2024-08-14 22:01:16 -0300

    Update User.js

--- User.js
+++ User.js
@@ -1,1 +1,21 @@
 
+class User{
+    constructor (fullname, email, password) {
+        this.fullname= fullname
+        this.email= email
+        this.password= password
+    }
+
+    login(email, password) {
+            
+        if(this.email === email && this.password === password){
+            console.log("Login bem sucedido!")
+        }
+        else{
+            console.log("Falha no login, tente novamente!")
+            }
+        }
+    }
+          
+const usuario = new User("Jefferson@gmail.com", "207024")
+usuario.login("Jefferson@gmail.com", "207024")
